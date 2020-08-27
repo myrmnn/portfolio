68d736474994ebc257583eeca7852b1edc0e35a3
myrmnn <myrm.nn@gmail.com>
2020-08-27 12:09:46 -0400

    adjusted widths on about page

--- app/main.js
+++ app/main.js
@@ -17,7 +17,7 @@ window.addEventListener('resize', function (){
     if(window.innerWidth < 768){
         robot.style.width = '50px';
     li.forEach(el => el.style.padding = '.6em');
-    } else if (window.innerWidth > 768){
+    } else if (window.innerWidth){
         robot.style.width = '75px';
     li.forEach(el => el.style.padding = '.75em');
 
