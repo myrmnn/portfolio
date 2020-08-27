2cc066dd992870fdb9c3e7fb3b53e49257c9154e
myrmnn <myrm.nn@gmail.com>
2020-08-27 11:17:27 -0400

    fixed header responsiveness, added font styles and link to projects on about page

--- app/main.js
+++ app/main.js
@@ -1,25 +1,25 @@
 //header
 const robot = document.querySelector('.robot');
-const li = document.querySelectorAll('li');
+const li = document.querySelectorAll('.header__li');
 
 
 document.addEventListener('scroll', function(){
   if(window.scrollY > 0 && window.innerWidth > 768){
       robot.style.width = '50px';
-    li.forEach(el => el.style.padding = '.5em');
+    li.forEach(el => el.style.padding = '.6em');
   } else if(window.scrollY < 30 && window.innerWidth > 768){
     robot.style.width = '75px';
-    li.forEach(el => el.style.padding = '1em');
+    li.forEach(el => el.style.padding = '.75em');
   }
 });
 
 window.addEventListener('resize', function (){
     if(window.innerWidth < 768){
         robot.style.width = '50px';
-    li.forEach(el => el.style.padding = '.5em');
+    li.forEach(el => el.style.padding = '.6em');
     } else if (window.innerWidth > 768){
         robot.style.width = '75px';
-    li.forEach(el => el.style.padding = '1em');
+    li.forEach(el => el.style.padding = '.75em');
 
     }
 })
