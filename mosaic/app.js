48cc98d3c595268db16d25727281fede93282359
myrmnn <myrm.nn@gmail.com>
2020-11-02 10:07:43 -0500

    updated grid project, added popup message

--- mosaic/app.js
+++ mosaic/app.js
@@ -1,7 +1,15 @@
-let shape = document.querySelectorAll('.square');
+const button = document.querySelector("button");
+const modal = document.querySelector(".modal");
 
-for (var i = 0; i < shape.length; i++) {
-	shape[i].addEventListener('click', function () {
-		this.classList.toggle('color');
-	});
-}
+button.addEventListener("click", () => modal.classList.add("dismiss"));
+
+const square = document.querySelectorAll(".square");
+square.forEach((el) => {
+  el.addEventListener("click", function () {
+    let rgb = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(
+      Math.random() * 255
+    )}, ${Math.floor(Math.random() * 255)})`;
+    el.style.background = rgb;
+    el.classList.add("color");
+  });
+});
