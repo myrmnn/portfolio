89e1cb0b6cb41f58e745abb374e6e9491a04ab20
myrmnn <myrm.nn@gmail.com>
2022-06-20 17:45:26 -0400

    removed old projects

--- app/main.js
+++ app/main.js
@@ -1,30 +1,30 @@
 //header
-const robot = document.querySelector('.robot');
-const li = document.querySelectorAll('.header__li');
-const menuBar = document.querySelector('.menu');
-const nav = document.querySelector('.header__list');
+const robot = document.querySelector(".robot");
+const li = document.querySelectorAll(".header__li");
+const menuBar = document.querySelector(".menu");
+const nav = document.querySelector(".header__list");
 
-menuBar.addEventListener('click', function () {
-	menuBar.classList.toggle('close');
-	nav.classList.toggle('show');
+menuBar.addEventListener("click", function () {
+	menuBar.classList.toggle("close");
+	nav.classList.toggle("show");
 });
 
-document.addEventListener('scroll', function () {
+document.addEventListener("scroll", function () {
 	if (window.scrollY > 0 && window.innerWidth > 768) {
-		robot.style.width = '50px';
-		li.forEach((el) => (el.style.padding = '.6em'));
+		robot.style.width = "50px";
+		li.forEach((el) => (el.style.padding = ".6em"));
 	} else if (window.scrollY < 30 && window.innerWidth > 768) {
-		robot.style.width = '75px';
-		li.forEach((el) => (el.style.padding = '.75em'));
+		robot.style.width = "75px";
+		li.forEach((el) => (el.style.padding = ".75em"));
 	}
 });
 
-window.addEventListener('resize', function () {
+window.addEventListener("resize", function () {
 	if (window.innerWidth < 768) {
-		robot.style.width = '50px';
-		li.forEach((el) => (el.style.padding = '.3em'));
+		robot.style.width = "50px";
+		li.forEach((el) => (el.style.padding = ".3em"));
 	} else if (window.innerWidth > 768) {
-		robot.style.width = '75px';
-		li.forEach((el) => (el.style.padding = '.75em'));
+		robot.style.width = "75px";
+		li.forEach((el) => (el.style.padding = ".75em"));
 	}
 });
